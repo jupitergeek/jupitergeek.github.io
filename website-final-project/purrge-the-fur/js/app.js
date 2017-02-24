62b1c977e4c27b31a4d5b680cd987033ea4e1dfc
Sarah Litwin <sarah.litwin@gmail.com>
2017-02-23 19:39:36 -0500

    final-project
    before presentations

--- website-final-project/purrge-the-fur/js/app.js
+++ website-final-project/purrge-the-fur/js/app.js
@@ -1,12 +1,28 @@
-$(document).ready(function {
+// $(document).ready(function {
 
-// Paw Scroll to top-of-page
-	$('#top, #down').on('click', function(e){
-    e.preventDefault();
-    var target= $(this).get(0).id == 'top' ? $('#down') : $('#top');
-    $('html, body').stop().animate({
-       scrollTop: target.offset().top
-    }, 1000);
-	});
+// // Paw Scroll to top-of-page
+// 	$('#top, #down').on('click', function(e){
+//     e.preventDefault();
+//     var target= $(this).get(0).id == 'top' ? $('#down') : $('#top');
+//     $('html, body').stop().animate({
+//        scrollTop: target.offset().top
+//     }, 1000);
+// 	});
 
-});
+// });
+
+// Image Carousel, Vanilla Javascript
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 3000); // Change image every 3 seconds
+}
